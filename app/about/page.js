689d5211f37069067b50bd1138a1af0ded97fd32
kakadais <github@codeasy.org>
2026-07-05 01:25:15 +0900

    Lecture rebuild checkpoint step 01

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>
+        This page will introduce the blog owner. For now, it is a simple page
+        shell used to practice Next.js routing.
+      </p>
+    </main>
+  );
+}
