b58bb33d8c72aafab0e0070d6e90832b694712c6
kakadais <github@codeasy.org>
2026-07-05 00:14:50 +0900

    Rebuild step-1 from main

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <main>
+      <h1>Contact Us</h1>
+      <p>The contact form will be added in a later step.</p>
+    </main>
+  );
+}
---
+++ app/post/page.js
@@ -0,0 +1,8 @@
+export default function NewPostPage() {
+  return (
+    <main>
+      <h1>Create New Post</h1>
+      <p>The post form will be added after the data and API layers are ready.</p>
+    </main>
+  );
+}
